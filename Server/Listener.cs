21c19fd48a486b9272de0234ad94e273516117b7
EdonFGashi <edongashi2004@outlook.com>
2024-11-12 23:02:17 +0100

    Implementimi i klases Listening tek server Side

---
+++ Server/Listener.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Listener
+    {
+        Socket s;
+        public bool Listening
+        {
+            get;
+            private set;
+        }
+
+        public int Port
+        {
+            get;
+            private set;
+        }
+
+        public Listener(int port)
+        {
+            Port = port;
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void setPort(int port)
+        {
+            Port = port;
+        }
+
+        public void Start()
+        {
+            if (Listening)
+                return;
+
+            s.Bind(new IPEndPoint(0, Port));
+            s.Listen(0);
+
+            //s.BeginAccept(callback, null);
+            Listening = true;
+        }
+
+        public void Stop()
+        {
+            if (!Listening)
+                return;
+
+            s.Close();
+            s.Dispose();
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+     
+    }
+}
