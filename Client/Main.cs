ac232827116764e620f540c0f949bac24a5790d0
ElonaFetahu <elona.fetahu@student.uni-pr.edu>
2024-11-14 13:29:45 +0100

    Fillimi i konfigurimit të socket-it për klientin dhe shtimi i event handler për përzgjedhjen e elementeve në ListView

--- Client/Main.cs
+++ Client/Main.cs
@@ -1,10 +1,30 @@
+using System.Net.Sockets;
+
 namespace Client
 {
+
+
+
+
     public partial class Main : Form
     {
+
+            Socket sck;
+    private string clientName = "";
+    private bool nameSent = false;  // Flag to check if name is sent
+
+    private CancellationTokenSource cancellationTokenSource;
+
+    
         public Main()
         {
             InitializeComponent();
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            listFiles.SelectedIndexChanged += ListView_ItemActivate;
+
         }
 
         private void Main_Load(object sender, EventArgs e)
@@ -12,4 +32,13 @@ namespace Client
 
         }
     }
+
+
+
+
+
+
+
+
+
 }
