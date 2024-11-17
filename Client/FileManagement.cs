584e728f83bf9111443eb68c2ec7e3f91f9fa3cd
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-11-17 15:42:20 +0100

    File managment

---
+++ Client/FileManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Connections_Server
+{
+    internal class FileManagement
+    {
+
+        //metoda per pranimin (download) te files
+        public void ReceiveFileFromServer(Socket clientSocket, string savePath)
+        {
+
+
+        }
+    }
+}
